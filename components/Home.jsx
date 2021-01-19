a7f1416eb1bf0e106bd27c8e5dbf80d2a17222c6
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-19 15:04:45 +0530

    added styles to home markup component

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,19 +1,22 @@
 import { Component } from "react";
 import Link from "next/link";
+import HomeStyles from "../styles/Index.module.css";
 
 class HomeMarkup extends Component {
   render() {
     return (
-      <div>
-        <Link href="/contentstack">
-          <button>Contentstack</button>
-        </Link>
-        <Link href="/surfboard">
-          <button>Surfboard</button>
-        </Link>
-        <Link href="/rawEngineering">
-          <button>Raw Engineering</button>
-        </Link>
+      <div className={HomeStyles.container}>
+        <div className={HomeStyles.buttonContainer}>
+          <Link href="/contentstack">
+            <button className={HomeStyles.button1}>Contentstack</button>
+          </Link>
+          <Link href="/surfboard">
+            <button className={HomeStyles.button2}>Surfboard</button>
+          </Link>
+          <Link href="/rawEngineering">
+            <button className={HomeStyles.button1}>Raw Engineering</button>
+          </Link>
+        </div>
       </div>
     );
   }
