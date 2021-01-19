31f937006d569f6a0e77e2115910e62f18e1a9c3
Rutuja9696 <rutuja.patil@contentstact.com>
2021-01-19 18:15:20 +0530

    added all company logo

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 import url from "../data/data";
+import Link from "next/link";
+
 //components
-import HomeMarkup from "../components/Home";
+// import HomeMarkup from "../components/Home";
+import HomeStyles from "../styles/Index.module.css";
 
 //function to render data on home page
 function Home(props) {
@@ -10,8 +13,31 @@ function Home(props) {
   return (
     <>
       <title>Home</title>
-      <div>
-        <HomeMarkup />
+      <div className={HomeStyles.container}>
+        <h2>Alone We Create...Together We Rule !</h2>
+        <h3>Explore more to know who we are...</h3>
+        <div className={HomeStyles.buttonContainer}>
+          <Link href="/contentstack">
+            <button className={HomeStyles.button1}>Contentstack</button>
+          </Link>
+          <Link href="/surfboard">
+            <button className={HomeStyles.button2}>Surfboard</button>
+          </Link>
+          <Link href="/rawEngineering">
+            <button className={HomeStyles.button1}>Raw Engineering</button>
+          </Link>
+        </div>
+        <div className={HomeStyles.logoContainer}>
+          {home.companies.map((company, i) => {
+            return (
+              <img
+                src={company.logo}
+                alt="company logo"
+                className={HomeStyles.logoImg}
+              />
+            );
+          })}
+        </div>
       </div>
     </>
   );
